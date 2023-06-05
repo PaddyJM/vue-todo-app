14b4cc9e664b544be0e6791bcb5fc826380dfc02
Patrick Morton <patrickjamesmorton@gmail.com>
2023-06-05 22:02:50 +0100

    initial commit

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+
+  defineProps<{msg: string}>()
+</script>
+
+
+<template>
+  <header>
+    <nav class="container">
+      <div class='branding'>
+        <img src="../assets/Vue_Logo_Black.png" />
+        <h1>{{ msg }}</h1>
+      </div>
+      <ul class="nav-routes">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style lang="scss" scoped></style>
