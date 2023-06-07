00153323e62ffd86094e8066b48c3a7666f73c2b
Patrick Morton <patrickjamesmorton@gmail.com>
2023-06-07 08:51:00 +0100

    got apigateway, lambda and dynamodb hooked up locally

--- api/handlers/createTodoHandler.ts
+++ api/handlers/createTodoHandler.ts
@@ -1,14 +1,27 @@
+import { DynamoDB } from "@aws-sdk/client-dynamodb";
 import { 
   APIGatewayProxyEvent, 
   APIGatewayProxyResult 
 } from "aws-lambda";
+
+const dynamoDBClient = new DynamoDB({
+  endpoint: `http://${process.env.LOCALSTACK_HOSTNAME}:4566`,
+  region: 'eu-west-2',
+});
 export const handler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
+  const response = await dynamoDBClient.putItem({
+    TableName: 'TodoApiStack-TodoTable585F1D6B-3a49e4a7',
+    Item: {
+      id: { S: '123' },
+      name: { S: 'test' }
+    }
+  });
   return {
     isBase64Encoded: false,
     statusCode: 200,
-    body: 'wahooo',
+    body: JSON.stringify(response),
     headers: {}
   }
 }
--- api/lib/api-stack.ts
+++ api/lib/api-stack.ts
@@ -1,6 +1,5 @@
-import { CfnOutput, Stack, StackProps, aws_apigateway } from 'aws-cdk-lib';
+import { CfnOutput, Stack, StackProps, aws_apigateway, aws_dynamodb } from 'aws-cdk-lib';
 import { RestApi } from 'aws-cdk-lib/aws-apigateway';
-import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
 import * as path from 'path';
@@ -14,13 +13,19 @@ export class TodoApiStack extends Stack {
       entry: path.join(__dirname, '../handlers/createTodoHandler.ts'),
     })
 
-    const api = new RestApi(this, 'TodoApi', {})
+    const api = new RestApi(this, 'TodoApi', {
+      cloudWatchRole: false,
+    })
 
     const lambdaIntegration = new aws_apigateway.LambdaIntegration(lambda)
 
     const resource = api.root.addResource('todo')
 
     resource.addMethod('GET', lambdaIntegration)
+
+    new aws_dynamodb.Table(this, 'TodoTable', {
+      partitionKey: { name: 'id', type: aws_dynamodb.AttributeType.STRING },
+    })
 
     new CfnOutput(this, "Endpoint", { value: `http://localhost:4566/restapis/${api.restApiId}/prod/_user_request_${resource.path}` })
   }
