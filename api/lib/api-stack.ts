0522e61410c9e42793d9733cafaeb77d11380060
Patrick Morton <patrickjamesmorton@gmail.com>
2023-06-08 09:02:05 +0100

    added some commented stuff for remote dpeloyment later

--- api/lib/api-stack.ts
+++ api/lib/api-stack.ts
@@ -1,20 +1,55 @@
-import { CfnOutput, Stack, StackProps, aws_apigateway, aws_dynamodb } from 'aws-cdk-lib';
-import { RestApi } from 'aws-cdk-lib/aws-apigateway';
-import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
-import { Construct } from 'constructs';
-import * as path from 'path';
+import {
+  CfnOutput,
+  Stack,
+  StackProps,
+  aws_apigateway,
+  aws_certificatemanager,
+  aws_dynamodb,
+  aws_ec2,
+  aws_route53
+} from 'aws-cdk-lib'
+import { RestApi } from 'aws-cdk-lib/aws-apigateway'
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
+import { Construct } from 'constructs'
+import * as path from 'path'
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class TodoApiStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
     const lambda = new NodejsFunction(this, 'createTodoHandler', {
-      entry: path.join(__dirname, '../handlers/createTodoHandler.ts'),
+      entry: path.join(__dirname, '../handlers/createTodoHandler.ts')
     })
+
+    // const vpc = new aws_ec2.Vpc(this, 'myVpc', {
+    //   cidr: '10.0.0.0/16',
+    //   maxAzs: 2,
+    //   subnetConfiguration: [
+    //     {
+    //       name: 'public',
+    //       subnetType: aws_ec2.SubnetType.PUBLIC,
+    //       cidrMask: 24
+    //     }
+    //   ]
+    // })
+
+    // const certificate = new aws_certificatemanager.Certificate(this, 'Certificate', {
+    //   domainName: 'todo-api',
+    //   validation: aws_certificatemanager.CertificateValidation.fromDns(
+    //     new aws_route53.PrivateHostedZone(this, 'HostedZone', {
+    //       zoneName: 'todo-api',
+    //       vpc
+    //     })
+    //   )
+    // })
 
     const api = new RestApi(this, 'TodoApi', {
       cloudWatchRole: false,
+      // domainName: {
+      //   domainName: 'todo-api',
+      //   certificate
+      // }
     })
 
     const lambdaIntegration = new aws_apigateway.LambdaIntegration(lambda)
@@ -25,9 +60,11 @@ export class TodoApiStack extends Stack {
 
     new aws_dynamodb.Table(this, 'TodoTable', {
       partitionKey: { name: 'id', type: aws_dynamodb.AttributeType.STRING },
-      tableName: 'TodoTable',
+      tableName: 'TodoTable'
     })
 
-    new CfnOutput(this, "Endpoint", { value: `http://localhost:4566/restapis/${api.restApiId}/prod/_user_request_${resource.path}` })
+    new CfnOutput(this, 'Endpoint', {
+      value: `http://localhost:4566/restapis/${api.restApiId}/prod/_user_request_${resource.path}`
+    })
   }
 }
