2bdf7babd14129f1771a1bcff54025e84732a9a6
Patrick Morton <patrickjamesmorton@gmail.com>
2023-06-08 08:18:33 +0100

    lint and added uuid for use later when users are introduced

--- api/handlers/createTodoHandler.ts
+++ api/handlers/createTodoHandler.ts
@@ -1,37 +1,33 @@
-import { DynamoDB } from "@aws-sdk/client-dynamodb";
-import { marshall } from "@aws-sdk/util-dynamodb";
-import { 
-  APIGatewayProxyEvent, 
-  APIGatewayProxyResult 
-} from "aws-lambda";
+import { DynamoDB } from '@aws-sdk/client-dynamodb'
+import { marshall } from '@aws-sdk/util-dynamodb'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import { v4 as uuid } from 'uuid'
 
 const dynamoDBClient = new DynamoDB({
   endpoint: `http://${process.env.LOCALSTACK_HOSTNAME}:4566`,
-  region: 'eu-west-2',
-});
-export const handler = async (
-  event: APIGatewayProxyEvent
-): Promise<APIGatewayProxyResult> => {
+  region: 'eu-west-2'
+})
+export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   if (!event.body) {
-    throw new Error('No body provided');
+    throw new Error('No body provided')
   }
-  const data = JSON.parse(event.body);
+  const data = JSON.parse(event.body)
   const record = marshall({
-    list: data.todos,
     id: "1",
-  });
-  let response;
-  let statusCode;
+    todos: data.todos
+  })
+  let response
+  let statusCode
   try {
     response = await dynamoDBClient.putItem({
       TableName: 'TodoTable',
-      Item: record,
-    });
-    statusCode = 200;
+      Item: record
+    })
+    statusCode = 200
   } catch (error) {
-    console.log(error);
-    response = error;
-    statusCode = 500;
+    console.log(error)
+    response = error
+    statusCode = 500
   }
   return {
     isBase64Encoded: false,
