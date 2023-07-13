b861a4e125847c2388193c83fb6a8376cc827938
Patrick Morton <patrickjamesmorton@gmail.com>
2023-07-13 17:46:36 +0100

    got multi environment deployments working

--- src/main.ts
+++ src/main.ts
@@ -9,9 +9,9 @@ app.use(router)
 app.use(
   createAuth0({
     domain: "dev-5akspl1d.us.auth0.com",
-    clientId: "pXQf8c3rDCqi7feJsn1MrXmb59i3HnB9",
+    clientId: `${import.meta.env.VITE_AUTH_0_CLIENT_ID}`,
     authorizationParams: {
-      redirect_uri: 'http://localhost:5173'
+      redirect_uri: `${import.meta.env.VITE_AUTH_0_REDIRECT_URI}`
     }
   })
 );
