6e416cffe81d1c6624a28d9670e3d6ceaf9135ec
Patrick Morton <patrickjamesmorton@gmail.com>
2023-12-08 13:57:04 +0000

    made creation of table conditional on its existence and added it to offline deploy script

--- infrastructure/scripts/create-table.sh
+++ infrastructure/scripts/create-table.sh
@@ -1,1 +1,15 @@
-aws dynamodb create-table --attribute-definitions AttributeName=id,AttributeType=S --table-name TodoTable-offline --key-schema AttributeName=id,KeyType=HASH --provisioned-throughput ReadCapacityUnits=10,WriteCapacityUnits=10 --endpoint-url http://localhost:8000
+if aws dynamodb describe-table --table-name TodoTable-offline --endpoint-url http://localhost:8000 > /dev/null
+then
+  echo "Table already exists"
+else
+  echo "Creating table"
+  aws dynamodb create-table \
+    --table-name TodoTable-offline \
+    --attribute-definitions \
+      AttributeName=id,AttributeType=S \
+    --key-schema \
+      AttributeName=id,KeyType=HASH \
+    --provisioned-throughput \
+      ReadCapacityUnits=10,WriteCapacityUnits=10 \
+    --endpoint-url http://localhost:8000
+fi
