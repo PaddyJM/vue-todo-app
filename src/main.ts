96c7dfce880b76a9683ee488b1f474e2f7eabe27
Patrick Morton <patrick.morton@huboo.com>
2023-12-12 19:23:14 +0000

    added vuetify to project

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,14 @@ import App from './App.vue'
 import router from './router'
 import { createAuth0 } from '@auth0/auth0-vue'
 import { createPinia } from 'pinia'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 const app = createApp(App)
 const pinia = createPinia()
+const vuetify = createVuetify({ components, directives })
 
 app.use(pinia)
 app.use(router)
@@ -18,5 +23,6 @@ app.use(
     }
   })
 )
+app.use(vuetify)
 
 app.mount('#app')
