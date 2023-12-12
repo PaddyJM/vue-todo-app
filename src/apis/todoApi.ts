28e97b8e7cb71f4c4b559ba53f8b11f43210581a
Patrick Morton <patrick.morton@huboo.com>
2023-12-12 14:47:59 +0000

    added proper type

--- src/apis/todoApi.ts
+++ src/apis/todoApi.ts
@@ -1,5 +1,10 @@
 import type { Todo } from '@/types'
 
+type TodoResponse = {
+  id?: string
+  todoList?: Todo[]
+}
+
 export default class TodoApi {
   async saveTodos(todos: Todo[], userId: string) {
     const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/client/${userId}/todo`, {
@@ -17,7 +22,7 @@ export default class TodoApi {
     return response.json()
   }
 
-  async loadTodos(userId: string): Promise<{ id?: string; todoList?: Todo[] }> {
+  async loadTodos(userId: string): Promise<TodoResponse> {
     const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/client/${userId}/todo`)
     if (!response.ok && response.status !== 404) {
       throw new Error('Error loading todos')
